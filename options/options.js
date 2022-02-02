7eb4892cc5605984ec376125c3d698dc6765cf79
Crowdbotics <team@crowdbotics.com>
2022-02-02 02:48:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "crypto_lottery_33311",
+  url: "https://crypto_lottery_33311.botics.co",
+  api: "https://crypto_lottery_33311.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
